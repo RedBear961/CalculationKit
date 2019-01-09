dd1d2ead229bc07ecb9b04e2a1ea96b9e7cb2884
RedBear961 <jora.bersenev@yandex.ru>
2019-01-09 23:18:43 +0700

    Initial Commit

---
+++ CalculationKit/CalculationKit.h
@@ -0,0 +1,19 @@
+//
+//  CalculationKit.h
+//  CalculationKit
+//
+//  Created by God on 09.01.2019.
+//  Copyright © 2019 WebView, Lab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CalculationKit.
+FOUNDATION_EXPORT double CalculationKitVersionNumber;
+
+//! Project version string for CalculationKit.
+FOUNDATION_EXPORT const unsigned char CalculationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculationKit/PublicHeader.h>
+
+
